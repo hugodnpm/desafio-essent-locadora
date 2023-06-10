e4f419a73dd3951e48c552827f1f9a9eef959398
hugodnpm <hugosc@gmail.com>
2023-06-09 22:38:25 -0300

    versão_entregue_0.2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
     if (!token && router.pathname !== '/') {
       router.push('/')
     }
-  }, [])
+  }, [token, router])
   return (
     <Layout>
       <Component {...pageProps} />
