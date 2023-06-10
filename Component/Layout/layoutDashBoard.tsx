c641b36efb484009cf16e147d08e1f7d5a026bfd
hugodnpm <hugosc@gmail.com>
2023-06-10 16:39:57 -0300

    versão_entregue_0.9

--- Component/Layout/layoutDashBoard.tsx
+++ Component/Layout/layoutDashBoard.tsx
@@ -109,7 +109,7 @@ const LayoutDashBoard = ({ children }: Props) => {
           <div className='px-6 my-6'>
             <button
               onClick={getOut}
-              className='flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple'
+              className='flex items-center justify-between w-full px-4 py-2 text-sm font-medium leading-5 text-black transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple'
             >
               Sair
             </button>
